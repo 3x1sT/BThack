b6b00bb56f112e4257fa3222e5adb30f67c87f8e
Ferra <ferra13671@gmail.com>
2024-07-11 01:32:48 +0300

    Delete src/main directory

--- src/main/java/com/bt/BThack/api/CommandSystem/Exception/VentureIllegalArgumentException.java
+++
@@ -1,6 +0,0 @@
-package com.bt.BThack.api.CommandSystem.Exception;
-
-public class VentureIllegalArgumentException extends VentureException {
-    public VentureIllegalArgumentException() { this(""); }
-    public VentureIllegalArgumentException(String message) { super(message); }
-}
--- src/main/java/com/bt/BThack/api/Utils/Interfaces/Fr.java
+++
@@ -1,8 +0,0 @@
-package com.bt.BThack.api.Utils.Interfaces;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-
-public interface Fr {
-    FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
-}
