7614d863df0c890c93a3d813826e569660e00047
Ferra <ferra13671@gmail.com>
2024-07-17 01:05:00 +0300

    Delete src/main directory

--- src/main/java/com/bt/BThack/api/CommandSystem/Exception/VentureBadArgumentSizeException.java
+++
@@ -1,6 +0,0 @@
-package com.bt.BThack.api.CommandSystem.Exception;
-
-public class VentureBadArgumentSizeException extends VentureIllegalArgumentException {
-    public VentureBadArgumentSizeException() { this(""); }
-    public VentureBadArgumentSizeException(String message) { super(message); }
-}
