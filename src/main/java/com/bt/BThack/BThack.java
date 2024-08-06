291d169d6b12f1db953fb665d2ee8f1e846edc49
Ferra <ferra13671@gmail.com>
2024-08-06 18:05:53 +0300

    1.5.0.2

--- src/main/java/com/bt/BThack/BThack.java
+++ src/main/java/com/bt/BThack/BThack.java
@@ -42,7 +42,7 @@ public class BThack implements Mc {
      */
     public static final String MODID = "bthack";
     public static final String NAME = "BThack Mod";
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.5.0.2";
     public static final String APP_ID = "1221431287852826676";
 
     //public static final String CLIENT_PROXY = "com.bt.Proxy.ClientProxy";
